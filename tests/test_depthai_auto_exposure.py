2e938557feaad4ed7a30695f77755f58c314e9ee
Duc Anh Ho <ducanh.ho2296@gmail.com>
2023-04-01 16:47:58 +0200

    update test

--- tests/test_depthai_auto_exposure.py
+++ tests/test_depthai_auto_exposure.py
@@ -8,11 +8,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from depthai_auto_exposure import AutoExposureRegion
 
-def test_camera():
-    cap = cv2.VideoCapture(0)
-    assert cap.isOpened() == True
-    cap.release()
-
 def test_depthai():
     with pytest.raises(RuntimeError, match="No available devices"):
         from depthai_auto_exposure import pipeline
